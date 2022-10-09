e637b1ce66688d4b951bc58cd05c99fa51674a26
AmetoX <xametox.officiall@yahoo.com>
2022-10-09 12:24:41 +0300

    pasul 1

--- TemaCriptografie/Teme/Tema1.cs
+++ TemaCriptografie/Teme/Tema1.cs
@@ -10,7 +10,43 @@ namespace TemaCriptografie
     {
         internal override void Problema()
         {
-            
+            Criptare();
+            DeCriptare();
+        }
+        static string t = "";
+        static int n = 3;
+        private void Criptare()
+        {
+            Console.Write("Text: ");
+            string text = Console.ReadLine();
+            int index = text.Length;
+            int count = 0;
+            if (text != null)
+            {
+                while (count != index)
+                {
+                    char a = text[count];
+                    char b = ' ';
+                    count++;
+                    if (a == b)
+                    {
+                        a = b;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            a++;
+                        }
+                    }
+                    t += a;
+                }
+            }
+            Console.WriteLine(t);
+        }
+        private void DeCriptare()
+        {
+
         }
     }
 }
