3cbf83fd513341f2f0412455507b26f724d21f5d
AmetoX <xametox.officiall@yahoo.com>
2022-11-14 16:57:39 +0200

    cilindru alpha v2

--- TemaCriptografie/Teme/Tema5.cs
+++ TemaCriptografie/Teme/Tema5.cs
@@ -10,6 +10,7 @@ namespace TemaCriptografie.Teme
     {
         int n;
         char[,] cilindru;
+        int[] key;
         internal override void Cripatare()
         {
             Console.Write("Numarul de discuri: ");
@@ -17,25 +18,67 @@ namespace TemaCriptografie.Teme
             Console.Write("Text: ");
             string text = Console.ReadLine();
             int index = text.Length;
+            key = new int[index];
+            string t = "";
+
+            //generare cilindru
             cilindru= new char[26,n];
             char a = 'a';
             for (int i = 0; i < 26; i++)
-            {
-                 
+            {                 
                 for (int j = 0; j < n; j++)
                 {
                     cilindru[i, j] = a;                   
                 }
                 a++;
             }
+            show(cilindru, n);
 
-
-            show(cilindru, 5);
+            //caut literele pe discuri
+            int count = 0;//literele
+            int contor = 0;//coloanele
+            while(contor != n) 
+            { 
+                for(int i = 0; i < 26; i++)
+                {
+                    if (count != index)
+                    {
+                        char b = text[count];
+                        if (contor != n && cilindru[i, contor] == b)
+                        {
+                            Console.WriteLine("i = " + i + " j= " + contor);
+                            key[contor] = i+1;
+                            if(cilindru[i, contor] == 'z')
+                            {
+                                t += 'a';
+                                key[contor] = 0;
+                            }
+                            else
+                            {
+                                t += cilindru[i+1, contor];
+                            }
+                            contor++;
+                            count++;                           
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            for(int i = 0; i< key.Length; i++)
+            {
+                Console.Write(key[i]+ " ");
+            }
+            Console.WriteLine();
+            for(int i = 0; i < t.Length; i++)
+            {
+                Console.Write(t[i]+ " ");
+            }
         }
 
         internal override void DeCripatare()
         {
-            
+
         }
 
         internal override void CriptoAnaliza()
